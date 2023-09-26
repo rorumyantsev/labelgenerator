ae6bf37c2738dbd5b64818ac87fe2fae6326609b
rorumyantsev <126847670+rorumyantsev@users.noreply.github.com>
2023-09-26 02:45:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -41,266 +41,7 @@ def index():
     </html>
     '''
 
-@app.route('/test', methods=['POST'])
-def test():
-    data = request.form
-    order_number = data['order_number']
-    name = data['name']
-    phone_number = data['phone']
-    destination_address = data['destination_address']
-    origin_address = data['source_address']
-    comment = data['comment']
-    qr = qrcode.QRCode(
-            version=1,
-            error_correction=qrcode.constants.ERROR_CORRECT_L,
-            box_size=10,
-            border=4,
-        )
-    qr.add_data(order_number)
-    qr.make(fit=True)
 
-    qr_img = qr.make_image(fill_color="black", back_color="white")
-    qr_img = qr_img.resize((100, 100))  # Resize the QR code to 100x100px
-
-    buffer = io.BytesIO()
-    qr_img.save(buffer, format="PNG")
-    qr_base64 = base64.b64encode(buffer.getvalue()).decode("utf-8")
-    return render_template_string('''
-            <!DOCTYPE html>
-            <html>
-            <head>
-                <title>Label</title>
-                <style>
-                    .label {
-                        font-family: Arial, sans-serif;
-                        text-align: center;
-                        margin: 10px;
-                        padding: 10px;
-                        width: 10cm;
-                        height: 10cm;
-                        border: 12px solid black;
-                        display: grid;
-                        grid-template-areas:
-                            "logo logo origin"
-                            "qr qr number"
-                            "qr qr name"
-                            ". . phone"
-                            ". . destination"
-                            ". . comment";
-                    }
-                    .logo {
-                        grid-area: logo;
-                        padding: 10px;
-                        text-align: left;
-                    }
-                    .logo img {
-                        width: 100px;
-                        height: 100px;
-                    }
-                    .number {
-                        grid-area: number;
-                        text-align: center;
-                    }
-                    .origin {
-                        grid-area: origin;
-                        text-align: center;
-                    }
-                    .destination {
-                        grid-area: destination;
-                        text-align: center;
-                    }
-                    .qr {
-                        grid-area: qr;
-                        text-align: center;
-                    }
-                    .name {
-                        grid-area: name;
-                        text-align: center;
-                    }
-                    .phone {
-                        grid-area: phone;
-                        text-align: center;
-                    }
-                    .comment {
-                        grid-area: comment;
-                        text-align: center;
-                    }
-                </style>
-            </head>
-            <body>
-                <div class="label">
-                    <div class="logo">
-                        <img src="yango_logo.png" alt="Yango Delivery Logo" />
-                    </div>
-                    <div class="number">
-                        <label>Número de Orden:</label><br>
-                        {{ order_number }}
-                    </div>
-                    <div class="origin">
-                        <label>Dirección de Origen:</label><br>
-                        {{ origin_address }}
-                    </div>
-                    <div class="destination">
-                        <label>Dirección de Destino:</label><br>
-                        {{ destination_address }}
-                    </div>
-                    <div class="qr">
-                        <img src="data:image/png;base64,{{ qr_base64 }}" alt="QR code" />
-                    </div>
-                    <div class="name">
-                        <label>Nombre:</label><br>
-                        {{ name }}
-                    </div>
-                    <div class="phone">
-                        <label>Número de teléfono:</label><br>
-                        {{ phone_number }}
-                    </div>
-                    <div class="comment">
-                        <label>Observaciones:</label><br>
-                        {{ comment }}
-                    </div>
-                </div>
-            </body>
-            </html>
-        ''',
-        order_number=order_number,
-        origin_address=origin_address,
-        destination_address=destination_address,
-        qr_base64=qr_base64,
-        name=name,
-        phone_number=phone_number,
-        comment=comment
-        )
-
-@app.route('/test2')
-def test2():
-    order_number = "1234"
-    name = "john doe"
-    phone_number = "+573111111111"
-    destination_address = "test address"
-    origin_address = "testadressssss"
-    comment = "comment"
-    qr = qrcode.QRCode(
-            version=1,
-            error_correction=qrcode.constants.ERROR_CORRECT_L,
-            box_size=10,
-            border=4,
-        )
-    qr.add_data(order_number)
-    qr.make(fit=True)
-
-    qr_img = qr.make_image(fill_color="black", back_color="white")
-    qr_img = qr_img.resize((100, 100))  # Resize the QR code to 100x100px
-
-    buffer = io.BytesIO()
-    qr_img.save(buffer, format="PNG")
-    qr_base64 = base64.b64encode(buffer.getvalue()).decode("utf-8")
-    return render_template_string('''
-            <!DOCTYPE html>
-            <html>
-            <head>
-                <title>Label</title>
-                <style>
-                    .label {
-                        font-family: Arial, sans-serif;
-                        text-align: center;
-                        margin: 10px;
-                        padding: 10px;
-                        width: 10cm;
-                        height: 10cm;
-                        border: 12px solid black;
-                        display: grid;
-                        grid-template-areas:
-                            "logo logo origin"
-                            "qr qr number"
-                            "qr qr name"
-                            ". . phone"
-                            ". . destination"
-                            ". . comment";
-                    }
-                    .logo {
-                        grid-area: logo;
-                        padding: 10px;
-                        text-align: left;
-                    }
-                    .logo img {
-                        width: 100px;
-                        height: 100px;
-                    }
-                    .number {
-                        grid-area: number;
-                        text-align: center;
-                    }
-                    .origin {
-                        grid-area: origin;
-                        text-align: center;
-                    }
-                    .destination {
-                        grid-area: destination;
-                        text-align: center;
-                    }
-                    .qr {
-                        grid-area: qr;
-                        text-align: center;
-                    }
-                    .name {
-                        grid-area: name;
-                        text-align: center;
-                    }
-                    .phone {
-                        grid-area: phone;
-                        text-align: center;
-                    }
-                    .comment {
-                        grid-area: comment;
-                        text-align: center;
-                    }
-                </style>
-            </head>
-            <body>
-                <div class="label">
-                    <div class="logo">
-                        <img src="yango_logo.png" alt="Yango Delivery Logo" />
-                    </div>
-                    <div class="number">
-                        <label>Número de Orden:</label><br>
-                        {{ order_number }}
-                    </div>
-                    <div class="origin">
-                        <label>Dirección de Origen:</label><br>
-                        {{ origin_address }}
-                    </div>
-                    <div class="destination">
-                        <label>Dirección de Destino:</label><br>
-                        {{ destination_address }}
-                    </div>
-                    <div class="qr">
-                        <img src="data:image/png;base64,{{ qr_base64 }}" alt="QR code" />
-                    </div>
-                    <div class="name">
-                        <label>Nombre:</label><br>
-                        {{ name }}
-                    </div>
-                    <div class="phone">
-                        <label>Número de teléfono:</label><br>
-                        {{ phone_number }}
-                    </div>
-                    <div class="comment">
-                        <label>Observaciones:</label><br>
-                        {{ comment }}
-                    </div>
-                </div>
-            </body>
-            </html>
-        ''',
-        order_number=order_number,
-        origin_address=origin_address,
-        destination_address=destination_address,
-        qr_base64=qr_base64,
-        name=name,
-        phone_number=phone_number,
-        comment=comment
-        )
 
 @app.route('/generate_label', methods=['POST'])
 def generate_label():
@@ -348,7 +89,7 @@ def generate_label():
                         padding: 10px;
                         width: 10cm;
                         height: 10cm;
-                        border: 12px solid black;
+                        border: 2px solid black;
                         display: grid;
                         grid-template-areas:
                             "logo logo origin"
@@ -414,9 +155,6 @@ def generate_label():
                         <label>Dirección de Destino:</label><br>
                         {{ destination_address }}
                     </div>
-                    <div class="qr">
-                        <img src="data:image/png;base64,{{ qr_base64 }}" alt="QR code" />
-                    </div>
                     <div class="name">
                         <label>Nombre:</label><br>
                         {{ name }}
@@ -428,6 +166,9 @@ def generate_label():
                     <div class="comment">
                         <label>Observaciones:</label><br>
                         {{ comment }}
+                    </div>
+                    <div class="qr">
+                        <img src="data:image/png;base64,{{ qr_base64 }}" alt="QR code" />
                     </div>
                 </div>
             </body>
